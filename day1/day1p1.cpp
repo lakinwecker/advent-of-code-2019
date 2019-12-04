5ac88c406a36cc0b2f7906e87b89eddbd698de0e
Lakin Wecker <lakin@structuredabstraction.com>
2019-12-03 22:01:42 -0700

    cppftw

--- day1/day1p1.cpp
+++ day1/day1p1.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <numeric>
 
 using namespace std;
 
+int fuel(int mass) {
+    return mass / 3 - 2;
+}
+
 int main() {
-    uint64_t mass, total = 0;
-    auto fuel = [](uint64_t mass) -> uint64_t { return mass/3-2; };
-    while(cin >> mass) {
-        total += fuel(mass);
-    }
-    cout << total << endl;
+    vector<string> lines;
+    copy(istream_iterator<string>(cin), istream_iterator<string>(), back_inserter(lines));
+    vector<int> masses(lines.size());
+    transform(lines.begin(), lines.end(), masses.begin(), [](auto l) {
+        istringstream out(l);
+        int i;
+        out >> i;
+        return fuel(i);
+    });
+    cout << accumulate(masses.begin(), masses.end(), 0) << endl;
 }
