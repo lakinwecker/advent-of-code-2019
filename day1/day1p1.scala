b7a4c94e8eafd6b96b5fdf12db342a9ba8609c24
Lakin Wecker <lakin@structuredabstraction.com>
2019-12-03 22:13:26 -0700

    maybe more idiomatic scala (without error checking)

--- day1/day1p1.scala
+++ day1/day1p1.scala
@@ -4,10 +4,7 @@ import scala.io.Source
 object Day1P1 extends App {
   val fuel = (mass: Int) => mass / 3 - 2
 
-  val total =
-    ((Source.fromFile(args(0)).getLines)
-      .foldLeft
-        (0)
-        ((total, line) => total + fuel(line.toInt)))
+  val lines = Source.fromFile(args(0)).getLines 
+  val total = lines.map(_.toInt).map(fuel).sum
   println(total)
 }
