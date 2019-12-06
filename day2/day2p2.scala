e4530d53a700c89b2c1bf649c590a362e8ec0522
Lakin Wecker <lakin@structuredabstraction.com>
2019-12-05 20:09:48 -0700

    Slightly better day2p2

--- day2/day2p2.scala
+++ day2/day2p2.scala
@@ -4,35 +4,45 @@ import scala.collection.immutable.Vector
 
 object Day2P2 extends App {
 
-  def process(idx: Int, opcodes: Vector[Int]): Int =
-    idx match {
-      case idx if idx > opcodes.length-4 => opcodes(0)
-      case idx => opcodes(idx) match {
-        case 99 => opcodes(0)
-        case 1 => process(idx+4, opcodes.updated(opcodes(idx+3), opcodes(opcodes(idx+1)) + opcodes(opcodes(idx+2))))
-        case 2 => process(idx+4, opcodes.updated(opcodes(idx+3), opcodes(opcodes(idx+1)) * opcodes(opcodes(idx+2))))
-      }
-    }
-  // Recurse instead of double for loop. Ick.
-  def tryAll(noun: Int, verb: Int, opcodes: Vector[Int]): (Int, Int) =
-    (noun, verb) match {
-      case (n, v) if n > 99 => (n, v)
-      case (n, v) if v > 99 => tryAll(n+1, 0, opcodes)
-      case (n, v) => 
-        process(0, opcodes.updated(1, n).updated(2, v)) match {
-          case 19690720 => (n, v)
-          case _ => tryAll(n, v+1, opcodes)
+  def process(idx: Int, opcodes: Vector[Int]): Option[Int] = {
+    val getVals = (idx1:Int, idx2:Int) => opcodes.lift(idx1).zip(opcodes.lift(idx2))
+    opcodes.slice(idx, idx+4) match {
+      case 99 +: rest => opcodes.lift(0)
+      case op +: rhsI +: lhsI +: target +: rest => 
+        (op, getVals(rhsI, lhsI)) match {
+          case (1, Some((rhs,lhs))) => process(idx+4, opcodes.updated(target, rhs + lhs))
+          case (2, Some((rhs,lhs))) => process(idx+4, opcodes.updated(target, rhs * lhs))
+          case _ => None
         }
+      case _ => None
     }
-  val opcodes = Vector.empty ++ Source.fromFile(args(0)).getLines
-      .filter(s => s.length() > 0)
-      .mkString(",")
-      .split(",")
-      .flatMap(_.toIntOption)
-
-  val answer = tryAll(0, 0, opcodes) match {
-    case (n, v) => 100 * n +v
   }
 
-  println(answer)
+  // https://stackoverflow.com/questions/8217764/cartesian-product-of-two-lists
+  def cartesianProduct[T](xss: List[List[T]]): List[List[T]] = xss match {
+    case Nil => List(Nil)
+    case h :: t => for(xh <- h; xt <- cartesianProduct(t)) yield xh :: xt
+  }
+  // Recurse instead of double for loop. Ick.
+  def tryAll(noun: Int, verb: Int, opcodes: Vector[Int]): Option[List[Int]] =
+    cartesianProduct(List((0 to 100).toList, (0 to 100).toList)).find(
+      _ match {
+        case (n +: v +: rest) => 
+          process(0, opcodes.updated(1, n).updated(2, v)) == Some(19690720)
+        case _ => false
+      }
+    )
+
+  val opcodes = Source
+      .fromFile(args(0))
+      .getLines
+      .filter(_.nonEmpty)
+      .flatMap(_.split(","))
+      .flatMap(_.toIntOption)
+      .toVector
+
+  tryAll(0, 0, opcodes) match {
+    case Some(n +: v +: List()) => println(100 * n +v)
+    case _ => println("Unable to find solution")
+  }
 }
