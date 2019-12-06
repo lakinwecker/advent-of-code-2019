a6bf8c76cd40bea85285ffc59c04502420226b32
Lakin Wecker <lakin@structuredabstraction.com>
2019-12-05 19:39:11 -0700

    Much better version of day2

--- day2/day2p1.scala
+++ day2/day2p1.scala
@@ -3,23 +3,32 @@ import scala.collection.immutable.Vector
 
 
 object Day2P1 extends App {
-  def process(idx: Int, opcodes: Vector[Int]): Int =
-    idx match {
-      case idx if idx > opcodes.length-4 => opcodes(0)
-      case idx => opcodes(idx) match {
-        case 99 => opcodes(0)
-        case 1 => process(idx+4, opcodes.updated(opcodes(idx+3), opcodes(opcodes(idx+1)) + opcodes(opcodes(idx+2))))
-        case 2 => process(idx+4, opcodes.updated(opcodes(idx+3), opcodes(opcodes(idx+1)) * opcodes(opcodes(idx+2))))
-      }
+  def process(idx: Int, opcodes: Vector[Int]): Option[Int] = {
+    val getVals = (idx1:Int, idx2:Int) => opcodes.lift(idx1).zip(opcodes.lift(idx2))
+    opcodes.slice(idx, idx+4) match {
+      case 99 +: rest => opcodes.lift(0)
+      case op +: lhsI +: rhsI +: target +: rest => 
+        (op, getVals(lhsI, rhsI)) match {
+          case (1, Some((lhs,rhs))) => process(idx+4, opcodes.updated(target, lhs + rhs))
+          case (2, Some((lhs,rhs))) => process(idx+4, opcodes.updated(target, lhs * rhs))
+          case _ => None
+        }
+      case _ => None
     }
-  val opcodes = Vector.empty ++ Source.fromFile(args(0)).getLines
-      .filter(s => s.length() > 0)
-      .mkString(",")
-      .split(",")
+  }
+
+  val opcodes = Source
+      .fromFile(args(0))
+      .getLines
+      .filter(_.nonEmpty)
+      .flatMap(_.split(","))
       .flatMap(_.toIntOption)
+      .toVector
       .updated(1, 12)
       .updated(2, 2)
 
-  val result = process(0, opcodes)
-  println(result)
+  process(0, opcodes) match {
+    case Some(result) => println(result)
+    case None => println("Bad Input")
+  }
 }
